3d6cac429620924b4780cb39734d7fc94f9c3b29
OP <ovidiu.ps@gmail.com>
2024-04-03 11:30:43 +0300

    Added new examples

---
+++ examples.js
@@ -0,0 +1,105 @@
+console.log("Test running");
+import { createSignal, createEffect, batch, createMemo } from "./index.js";
+
+// [[ CREATE EFFECT ]]
+// When signal getters are called inside a create effect callback
+// that function is stored by the signal and it reruns whenever the
+// value is changed via the signal setter
+(() => {
+  const button = document.querySelector("#createEffect-button");
+  const [count, setCount] = createSignal(0);
+
+  createEffect(() => {
+    console.log("Count is now", count());
+    button.innerText = `Counter: ${count()}`;
+  });
+
+  button.addEventListener("click", () => setCount(count() + 1));
+})();
+
+// ==============================================================================
+
+// [[ CREATE MEMO ]]
+(() => {
+  const memobutton = document.querySelector("#memo-button");
+  const nomemobutton = document.querySelector("#no-memo-button");
+  const memotext = document.querySelector("#memo-text");
+  const nomemotext = document.querySelector("#no-memo-text");
+  const [noMemoCount, setNoMemoCount] = createSignal(0);
+  const [memoCount, setMemoCount] = createSignal(0);
+
+  function countTo(nr, startNr = 0) {
+    console.log("Counting down", nr);
+    if (startNr > nr) {
+      console.log(" ------ Counting done ");
+      return nr;
+    }
+    return countTo(nr, startNr + 1);
+  }
+
+  // Without createMemo
+  createEffect(() => {
+    nomemobutton.innerText = `Without Memo Counter: ${noMemoCount()}`;
+    const fragment = document.createDocumentFragment();
+    console.log(" ========= Without createMemo Start ========= ");
+    for (let i = 0; i <= noMemoCount(); i++) {
+      fragment.append(document.createTextNode(` ${countTo(noMemoCount())}`));
+    }
+    console.log(" ========= Without createMemo End =========== ");
+    nomemotext.appendChild(fragment);
+  });
+
+  // With createMemo
+  const memoizedCountDown = createMemo(() => {
+    console.log("In CreateMemo");
+    return countTo(memoCount());
+  });
+
+  createEffect(() => {
+    console.log(" ========= With createMemo Start ========= ");
+    memobutton.innerText = `With Memo Counter: ${memoCount()}`;
+    const fragment = document.createDocumentFragment();
+    for (let i = 0; i <= memoCount(); i++) {
+      fragment.append(document.createTextNode(` ${memoizedCountDown()}`));
+    }
+    memotext.appendChild(fragment);
+    console.log(" ========= With createMemo End =========== ");
+  });
+
+  nomemobutton.addEventListener("click", () => {
+    setNoMemoCount(noMemoCount() + 1);
+  });
+  memobutton.addEventListener("click", () => {
+    setMemoCount(memoCount() + 1);
+  });
+})();
+// ==============================================================================
+
+// [[ BATCH ]]
+// When there are multiple signals in an effect, each of those signals
+// runs it's own copy of that effect. Wraping the setters in a batch
+// alows that effect to run only once
+(() => {
+  const batchbutton = document.querySelector("#batch-button");
+  const nobatchbutton = document.querySelector("#no-batch-button");
+  const [positiveCount, setPositiveCount] = createSignal(0);
+  const [negativeCount, setNegativeCount] = createSignal(0);
+
+  createEffect(() => {
+    console.log("Positive count is now", positiveCount());
+    console.log("Negative count is now", negativeCount());
+    batchbutton.innerText = `Batch Counter: ${positiveCount()}${negativeCount()}`;
+    nobatchbutton.innerText = `No Batch Counter: ${positiveCount()}${negativeCount()}`;
+  });
+
+  nobatchbutton.addEventListener("click", () => {
+    setPositiveCount(positiveCount() + 1);
+    setNegativeCount(negativeCount() - 1);
+  });
+  batchbutton.addEventListener("click", () => {
+    batch(() => {
+      setPositiveCount(positiveCount() + 1);
+      setNegativeCount(negativeCount() - 1);
+    });
+  });
+})();
